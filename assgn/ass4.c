7fafd8b90b060483b1e981426bb7fb84755140dd
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-21 20:07:17 +0530

    21-04-2025 Mon

--- assgn/ass4.c
+++ assgn/ass4.c
@@ -16,28 +16,32 @@ main()
 		if(sw1==0)
 		{
 			lcd_cmd(0X01);
+			lcd_cmd(0X80);
 			while(sw1==0); 
 			c++;
-			lcd_data(c);
-			delay_ms(200);
-			
+			lcd_data((c/10)+48);
+			lcd_data((c%10)+48);	
 		}
 		else if(sw2==0)
 		{
 			lcd_cmd(0X01);
+			lcd_cmd(0X80);
 		 	while(sw2==0);
 			c--;
-			lcd_data(c);
-			delay_ms(200);
+			lcd_data((c/10)+48);
+			lcd_data((c%10)+48);
 		}
 		else if(sw3==0)
 		{
-			lcd_cmd(0X01);a
+			lcd_cmd(0X01);
+			lcd_cmd(0X80);                                          
 		 	while(sw3==0);
 			while(c)
 			{
-				lcd_data(c--);
+				lcd_data((c/10)+48);
+				lcd_data((c%10)+48);
 				delay_ms(200);
+				c--;
 				lcd_cmd(0X01);
 			}
 		}
